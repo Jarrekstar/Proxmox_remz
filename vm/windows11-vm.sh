19a778ca02c7a24e5337f30f886a84721561d79d
Rémi Bédard-Couture <remi.bc@outlook.com>
2024-07-03 23:34:28 -0400

    Let qm create allocate disks dynamically; Add PowerShell script to install TightVNC

--- vm/windows11-vm.sh
+++ vm/windows11-vm.sh
@@ -562,14 +562,14 @@ for i in {0..2}; do
 done
 
 msg_info "Creating a Windows 11 VM"
-pvesm alloc $STORAGE $VMID $DISK0 4M 1>&/dev/null
-pvesm alloc $STORAGE $VMID $DISK1 ${DISK_SIZE}G 1>&/dev/null
-pvesm alloc $STORAGE $VMID $DISK2 4M 1>&/dev/null
+#pvesm alloc $STORAGE $VMID $DISK0 4M 1>&/dev/null
+#pvesm alloc $STORAGE $VMID $DISK1 ${DISK_SIZE}G 1>&/dev/null
+#pvesm alloc $STORAGE $VMID $DISK2 4M 1>&/dev/null
 qm create $VMID -agent 1${MACHINE} -bios ovmf${CPU_TYPE} -cores $CORE_COUNT -cpuunits 5000 -balloon $MIN_RAM_SIZE -memory $RAM_SIZE \
   -name $HN -tags proxmox-helper-scripts -net0 virtio,bridge=$BRG,macaddr=$MAC$VLAN$MTU,firewall=1 -ostype win11 -scsihw virtio-scsi-single \
-  -efidisk0 ${DISK0_REF}${FORMAT},pre-enrolled-keys=1 \
-  -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=${DISK_SIZE}G \
-  -tpmstate0 ${DISK2_REF},size=4M,version=v2.0 \
+  -efidisk0 ${STORAGE}:1,efitype=4m,pre-enrolled-keys=1 \
+  -scsi0 ${STORAGE}:${DISK_SIZE},${DISK_CACHE}${THIN} \
+  -tpmstate0 ${STORAGE}:1,version=v2.0 \
   -sata0 /var/lib/vz/template/iso/${WIN11_ISO},media=cdrom \
   -sata1 /var/lib/vz/template/iso/${VIRTIO_ISO},media=cdrom \
   -smbios1 uuid=$(od -x /dev/urandom | head -1 | awk '{OFS="-"; print $2$3,$4,$5,$6,$7$8$9}') \
